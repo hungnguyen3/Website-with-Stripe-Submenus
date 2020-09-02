af80173889f3ea8748aab5e93ee2b5eefab00fc8
hunglnl01 <hunglnl01@gmail.com>
2020-09-02 00:30:27 -0700

    adding submenus as we hover over the navbar

--- app.js
+++ app.js
@@ -37,3 +37,49 @@ sidebar.innerHTML = sublinks.map((item)=>{
     </div>
     </article>`
 }).join("");
+
+linkBtns.forEach((btn)=>{
+    btn.addEventListener('mouseover', function(event){
+        const text = event.currentTarget.textContent;
+        const tempBtn = event.currentTarget.getBoundingClientRect();
+        const center = (tempBtn.left + tempBtn.right)/2;
+        const bottom = tempBtn.bottom - 3;
+
+        const tempPage = sublinks.find(({page})=>page === text);
+
+        if(tempPage){
+            const {page,links} = tempPage;
+            submenu.classList.add('show');
+            submenu.style.left = `${center}px`;
+            submenu.style.top = `${bottom}px`;
+
+            let columns = 'col-2';
+            if(links.length === 3){
+                columns = 'col-3';
+            }else if(links.length > 3){
+                columns = 'col-4';
+            }
+
+            submenu.innerHTML = `<section>
+                                    <h4>${page}</h4>
+                                    <div class = "submenu-center ${columns}">
+                                        ${links.map((link)=>{
+                                            return `<a href = "${link.url}">
+                                            <i class="${link.icon}"></i>${link.label}
+                                            </a>`
+                                        }).join('')}
+                                    </div>
+                                </section>`
+        }
+    });
+});
+
+hero.addEventListener('mouseover', function(e){
+    submenu.classList.remove('show');
+});
+
+nav.addEventListener('mouseover', function(e){
+    if(!e.target.classList.contains('link-btn')){
+        submenu.classList.remove('show');
+    }
+});
