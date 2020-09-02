5aaab44b7c588f71702d944da9837ecf6a2f14d0
hunglnl01 <hunglnl01@gmail.com>
2020-09-01 23:19:36 -0700

    adding sublinks into the sidebar

--- app.js
+++ app.js
@@ -14,8 +14,26 @@ const nav = document.querySelector('.nav');
 toggleBtn.addEventListener('click', ()=>{
     // add a class list 'show'
     sidebarWrapper.classList.add('show');
-})
+});
 
 closeBtn.addEventListener('click', ()=>{
     sidebarWrapper.classList.remove('show');
-})
+});
+
+// set sidebar
+// sublinks has 3 items and for each items
+// map func does iterate over a parent array
+sidebar.innerHTML = sublinks.map((item)=>{
+    // set the link and page fields accordingly
+    const {links,page} = item;
+    return `<article>
+    <h4>${page}</h4>
+    <div class = "sidebar-sublinks">
+    ${links.map((link)=>{
+        return `<a href = "${link.url}">
+        <i class="${link.icon}"></i>${link.label}
+        </a>`
+    }).join('')}
+    </div>
+    </article>`
+}).join("");
